96b6e9200c2e737ab3f0d61382349586d75ba81a
Dimannn <diman1992nn@gmail.com>
2023-02-04 11:56:11 +0300

    add newPosterManagerTests

--- src/main/java/ru/netology/PosterManager.java
+++ src/main/java/ru/netology/PosterManager.java
@@ -21,6 +21,7 @@ public class PosterManager {
     }
 
     public String[] findAll() {
+
         return films;
     }
 
--- src/test/java/PosterManagerTest.java
+++ src/test/java/PosterManagerTest.java
@@ -40,6 +40,100 @@ public class PosterManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void ShouldFindLastAddFilms1() { // Тестируем граничные значения, когда добавлен 1 фильм
+        PosterManager manager = new PosterManager();
+        manager.addFilm("Film I");
 
+        String[] expected = {"Film I"};
+        String[] actual = manager.findLast();
 
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldFindLastAddFilms2() { // Тестируем граничные значения, когда добавлены 9 фильмов
+        PosterManager manager = new PosterManager();
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+        manager.addFilm("Film IV");
+        manager.addFilm("Film V");
+        manager.addFilm("Film VI");
+        manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+
+        String[] expected = {"Film IX", "Film VIII", "Film VII", "Film VI", "Film V", "Film IV", "Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+
+    public void ShouldFindLastAddFilms3() { // Тестируем граничные значения, когда добавлены 10 фильмов
+        PosterManager manager = new PosterManager();
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+        manager.addFilm("Film IV");
+        manager.addFilm("Film V");
+        manager.addFilm("Film VI");
+        manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+        manager.addFilm("Film X");
+
+        String[] expected = {"Film X", "Film IX", "Film VIII", "Film VII", "Film VI", "Film V", "Film IV", "Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+
+    public void ShouldFindLastAddFilms4() { // Тестируем граничные значения, когда добавлены 11 фильмов
+        PosterManager manager = new PosterManager();
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+        manager.addFilm("Film IV");
+        manager.addFilm("Film V");
+        manager.addFilm("Film VI");
+        manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+        manager.addFilm("Film X");
+        manager.addFilm("Film XI");
+
+        String[] expected = {"Film XI", "Film X", "Film IX", "Film VIII", "Film VII", "Film VI", "Film V", "Film IV", "Film III", "Film II"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+
+    public void ShouldFindLastAddFilms5() { // Тестируем значения, превышающие установленный лимит, когда добавлены 15 фильмов
+        PosterManager manager = new PosterManager();
+        manager.addFilm("Film I");
+        manager.addFilm("Film II");
+        manager.addFilm("Film III");
+        manager.addFilm("Film IV");
+        manager.addFilm("Film V");
+        manager.addFilm("Film VI");
+        manager.addFilm("Film VII");
+        manager.addFilm("Film VIII");
+        manager.addFilm("Film IX");
+        manager.addFilm("Film X");
+        manager.addFilm("Film XI");
+        manager.addFilm("Film XII");
+        manager.addFilm("Film XIII");
+        manager.addFilm("Film XIV");
+        manager.addFilm("Film XV");
+
+        String[] expected = {"Film XV", "Film XIV","Film XIII", "Film XII", "Film XI", "Film X", "Film IX", "Film VIII", "Film VII", "Film VI"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
